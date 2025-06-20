b2bdf7d0bc98a24c3703c1e07576e3106df49563
ym-pett <ympettinato@gmail.com>
2025-06-20 12:06:55 +0100

    comments to v2

--- src/tail_recursion_v2.py
+++ src/tail_recursion_v2.py
@@ -1,18 +1,35 @@
 class Recurse(Exception):
-    def __init__(self, *args, **kwargs):
+    """
+    custom Recurs exception. stores the arguments of the function in the 
+    exception object
+    """
+    def __init__(self, *args, **kwargs):  
         self.args = args
         self.kwargs = kwargs
 
 def recurse(*args, **kwargs):
-    raise Recurse(*args, **kwargs)
+    """
+    instead of calling function recursively, raises a Recurse exception 
+    with the new arguments interrupts the normal flow & signals to the 
+    decorator to recurse with these arguments
+    """
+    raise Recurse(*args, **kwargs) 
         
-def tail_recursive_v2(f):
-    def decorated(*args, **kwargs):
-        while True:
+def tail_recursive_v2(f): 
+    def decorated(*args, **kwargs): 
+        while True: # infinite loop which gets interrupted every time a new Recurse exception is raised 
             try:
-                return f(*args, **kwargs)
-            except Recurse as r:
+                return f(*args, **kwargs) # returns value when base case is reached & no longer calling a function
+            except Recurse as r: # unpacks the arguments carried over by raising Recurse
                 args = r.args
                 kwargs = r.kwargs
                 continue
-    return decorated
+    return decorated  
+
+# @tail_recursive_v2
+# def factorial(n, accumulator=1):
+#     if n == 0:
+#         return accumulator # exit condition
+#     recurse(n-1, accumulator=accumulator*n)
+
+# factorial(2)
